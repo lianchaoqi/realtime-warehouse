2da7913066d92ad47f61e2c25501b945873d94f1
jack <2504810905@qq.com>
2024-04-14 10:38:57 +0800

    Initial commit

---
+++ realtime-common/src/main/java/com/jack/gmall/realtime/common/util/T.java
@@ -0,0 +1,15 @@
+package com.jack.gmall.realtime.common.util;
+
+/**
+ * @BelongsProject: realtime-warehouse
+ * @BelongsPackage: com.jack.gmall.realtime.common.util
+ * @Author: lianchaoqi
+ * @CreateTime: 2024-04-14  00:48
+ * @Description: ~~~~
+ * @Version: jdk1.8
+ */
+public class T {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
